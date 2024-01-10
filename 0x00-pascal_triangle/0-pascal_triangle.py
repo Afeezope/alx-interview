fef638f626c7f36f7db4c3fcc59c58178e5c764f
Afeezope <afeezope1994@gmail.com>
2024-01-10 22:49:26 +0000

    0x00-pascal_triangle file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+# Example usage:
+# Uncomment the code below to test the function
+"""
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
+"""
+
